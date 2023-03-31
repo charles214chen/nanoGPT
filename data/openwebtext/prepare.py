543243e2d6ffc6e4cb9b3172361ddfba25e7f4de
charles214chen <chenchao214@outlook.com>
2023-03-31 22:28:21 +0800

    [refactor] just reformat

--- data/openwebtext/prepare.py
+++ data/openwebtext/prepare.py
@@ -5,7 +5,7 @@ import os
 from tqdm import tqdm
 import numpy as np
 import tiktoken
-from datasets import load_dataset # huggingface datasets
+from datasets import load_dataset  # huggingface datasets
 
 # number of workers in .map() call
 # good number to use is ~order number of cpu cores // 2
@@ -16,7 +16,7 @@ dataset = load_dataset("openwebtext")
 
 # owt by default only contains the 'train' split, so create a test split
 split_dataset = dataset["train"].train_test_split(test_size=0.0005, seed=2357, shuffle=True)
-split_dataset['val'] = split_dataset.pop('test') # rename the test split to val
+split_dataset['val'] = split_dataset.pop('test')  # rename the test split to val
 
 # this results in:
 # >>> split_dataset
@@ -33,12 +33,15 @@ split_dataset['val'] = split_dataset.pop('test') # rename the test split to val
 
 # we now want to tokenize the dataset. first define the encoding function (gpt2 bpe)
 enc = tiktoken.get_encoding("gpt2")
+
+
 def process(example):
-    ids = enc.encode_ordinary(example['text']) # encode_ordinary ignores any special tokens
-    ids.append(enc.eot_token) # add the end of text token, e.g. 50256 for gpt2 bpe
+    ids = enc.encode_ordinary(example['text'])  # encode_ordinary ignores any special tokens
+    ids.append(enc.eot_token)  # add the end of text token, e.g. 50256 for gpt2 bpe
     # note: I think eot should be prepended not appended... hmm. it's called "eot" though...
     out = {'ids': ids, 'len': len(ids)}
     return out
+
 
 # tokenize the dataset
 tokenized = split_dataset.map(
@@ -52,13 +55,13 @@ tokenized = split_dataset.map(
 for split, dset in tokenized.items():
     arr_len = np.sum(dset['len'])
     filename = os.path.join(os.path.dirname(__file__), f'{split}.bin')
-    dtype = np.uint16 # (can do since enc.max_token_value == 50256 is < 2**16)
+    dtype = np.uint16  # (can do since enc.max_token_value == 50256 is < 2**16)
     arr = np.memmap(filename, dtype=dtype, mode='w+', shape=(arr_len,))
 
     print(f"writing {filename}...")
     idx = 0
     for example in tqdm(dset):
-        arr[idx : idx + example['len']] = example['ids']
+        arr[idx: idx + example['len']] = example['ids']
         idx += example['len']
     arr.flush()
 
